df7142521aa8cf6a50197bdf37dfb92651ffd4d1
Omar Garcia <omar.garcia@trinnova.mx>
2023-11-14 12:01:50 -0600

    Agregando una cuadricula de IMG

--- App.js
+++ App.js
@@ -88,6 +88,38 @@ const App = () => {
           </View>
         </View>
 
+        <Text style={styles.titulo}>Hospedajes en LA</Text>
+
+        <View style={styles.listado}>
+          <View style={styles.listadoItem}>
+            <Image
+              style={styles.mejores}
+              source={ require('./assets/img/hospedaje1.jpg') }
+            />
+          </View>
+
+          <View style={styles.listadoItem}>
+            <Image
+              style={styles.mejores}
+              source={ require('./assets/img/hospedaje2.jpg') }
+            />
+          </View>
+
+          <View style={styles.listadoItem}>
+            <Image
+              style={styles.mejores}
+              source={ require('./assets/img/hospedaje3.jpg') }
+            />
+          </View>
+
+          <View style={styles.listadoItem}>
+            <Image
+              style={styles.mejores}
+              source={ require('./assets/img/hospedaje4.jpg') }
+            />
+          </View>
+        </View>
+
       </ScrollView>
     </>
   );
@@ -121,7 +153,18 @@ const styles = StyleSheet.create
       width: '100%',
       height: 200,
       marginVertical: 5,
-    },  
+    },
+
+    listado: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'space-between',
+    },
+
+    listadoItem: {
+      flexBasis: '49%',
+      padding: 10,
+    },
   }
 );
 
